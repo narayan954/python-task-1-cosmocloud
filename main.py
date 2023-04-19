93da9f0cfbcfe717c8f397b86b3479512c0f422d
Narayan soni <narayansoni854@gmail.com>
2023-04-20 01:56:12 +0530

    solved task in main.py

--- main.py
+++ main.py
@@ -26,7 +26,22 @@ def merge_lists(list_1, list_2) -> list:
     - Both lists are unsorted
     - Both lists can have missing values (for ex list_2 has missing id=2)
     """
-    # return list_3
+    # Create a temporary dictionary to store the information
+    merge_dict = dict()
+    
+    # Traverse and store the first list of dictionaries completely in dictionary
+    for i in list_1:
+        merge_dict[i["id"]]=i
+    
+    # Traverse the second list and update/merge with information already contained in merge_dict dictionary
+    for i in list_2:
+        if merge_dict[i["id"]]:
+            merge_dict[i["id"]].update(i)
+        else:
+            merge_dict[i["id"]]=i
+    
+    # return merged list from the values of merge_dict dictionary
+    return list(merge_dict.values())
 
 
 list_3 = merge_lists(list_1, list_2)
